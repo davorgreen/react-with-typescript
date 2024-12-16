3273a41f3f4d4de67ace3dc17097edf54927d0e9
Davor Grinfelder <grinfelderdavor@gmail.com>
2024-12-16 11:34:11 +0100

    ts

--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -1,3 +1,5 @@
+import { formatDuration } from '../utils/formatDuration';
+
 type VideoGridProps = {
 	id: string;
 	title: string;
@@ -13,7 +15,11 @@ type VideoGridProps = {
 	videoUrl: string;
 };
 
-const VideoGrid: React.FC = ({
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, {
+	notation: 'compact',
+});
+
+const VideoGrid: React.FC<VideoGridProps> = ({
 	id,
 	title,
 	channel,
@@ -32,9 +38,31 @@ const VideoGrid: React.FC = ({
 					className='block w-96 h-full object-cover rounded-xl'
 				/>
 				<div className='absolute top-0 bg-secondary-dark text-secondary text-sm px-5 rounded-xl'>
-					{duration}
+					{formatDuration(duration)}
 				</div>
 			</a>
+			<div className='flex gap-2'>
+				<a href={`/@${channel.id}`}>
+					<img
+						src={channel.profileUrl}
+						alt=''
+						className='w-12 h-12 rounded-full'
+					/>
+				</a>
+				<div className='flex flex-col'>
+					<a href={`/watch?v=${id}`} className='font-bold'>
+						{title}
+					</a>
+					<a
+						href={`/watch?v=${id}`}
+						className='font-bold text-secondary-text text-sm'>
+						{channel.name}
+					</a>
+					<div className='text-secondary-text text-sm'>
+						{VIEW_FORMATTER.format(views)} Views *
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
